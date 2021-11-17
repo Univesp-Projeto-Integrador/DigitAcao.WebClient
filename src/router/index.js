bc5c012a8d1d46611c6841247cea3c8a3a5414cc
Diego Marques <dmosantos@gmail.com>
2021-11-17 00:08:39 -0300

    Redirect home to login

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home
+        component: Home,
+        redirect: '/user/login'
     },
     {
         path: '/about',
